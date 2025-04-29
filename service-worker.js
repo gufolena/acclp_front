2c49691cc574f9afe55830575da9bab142c0c7ee
gufolena <gustavo.araujo@fabricadenegocio.com.br>
2025-04-29 15:18:55 -0300

    teste

--- service-worker.js
+++ service-worker.js
@@ -57,35 +57,10 @@ const urlsToCache = [
 
 ];
 
-// Instalação
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-// Ativação (limpa caches antigos)
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames
-          .filter(cacheName => cacheName !== CACHE_NAME)
-          .map(cacheName => caches.delete(cacheName))
-      )
-    )
-  );
-});
-
-// Intercepta requisições
-self.addEventListener('fetch', event => {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const clone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-          return response;
-        })
-        .catch(() => caches.match(event.request))
-    );
+    self.skipWaiting(); // ativa imediatamente
+  });
+  
+  self.addEventListener('activate', event => {
+    clients.claim(); // assume o controle das páginas abertas
   });
