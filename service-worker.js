e5eca2304d09afebc59f992504748cbafddbe00a
gufolena <gustavo.araujo@fabricadenegocio.com.br>
2025-04-29 15:12:27 -0300

    teste

--- service-worker.js
+++ service-worker.js
@@ -79,9 +79,13 @@ self.addEventListener('activate', event => {
 
 // Intercepta requisições
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  });
